c1a82b8ed3b29fc6220db975bed59dea5f4061fc
apple <wxj827@foxmail.com>
2019-06-20 14:38:34 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"gin-blog/pkg/setting"
+	"gin-blog/routers"
+	"net/http"
+)
+
+func main() {
+	//router := gin.Default()
+	//router.GET("/test", func(c *gin.Context) {
+	//	c.JSON(200, gin.H{
+	//		"message": "test",
+	//	})
+	//})
+	router := routers.InitRouter()
+
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	_ = s.ListenAndServe()
+}
