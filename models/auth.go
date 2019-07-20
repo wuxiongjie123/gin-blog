9e165314ef651382d5bd307016970e33292ba51c
apple <wxj827@foxmail.com>
2019-07-20 11:37:38 +0800

    go-auth-upload

--- models/auth.go
+++ models/auth.go
@@ -1,16 +1,19 @@
 package models
 
 type Auth struct {
-	ID int `gorm:"primary_key" json:"id"`
+	ID       int    `gorm:"primary_key" json:"id"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
-func CheckAuth(username, password string) bool {
+func CheckAuth(username, password string) (bool, error) {
 	var auth Auth
-	db.Select("id").Where(Auth{Username:username,Password:password}).First(&auth)
-	if auth.ID >0 {
-		return true
+	err := db.Select("id").Where(Auth{Username: username, Password: password}).First(&auth).Error
+	if err != nil {
+		return false, err
 	}
-	return false
+	if auth.ID > 0 {
+		return true, nil
+	}
+	return false, nil
 }
--- routers/api/auth.go
+++ routers/api/auth.go
@@ -1,10 +1,11 @@
 package api
 
 import (
-	"gin-blog/models"
+	"gin-blog/pkg/app"
 	"gin-blog/pkg/e"
-	"gin-blog/pkg/logging"
 	"gin-blog/pkg/util"
+	"gin-blog/service/auth_service"
+
 	"github.com/astaxie/beego/validation"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -15,37 +16,73 @@ type auth struct {
 	Password string `valid:"Required; MaxSize(50)"`
 }
 
-func GetAuth(c *gin.Context)  {
+func GetAuth(c *gin.Context) {
+	//username := c.Query("username")
+	//password := c.Query("password")
+	//
+	//valid := validation.Validation{}
+	//a := auth{Username:username,Password:password}
+	//ok, _:= valid.Valid(&a)
+	//
+	//data := make(map[string]interface{})
+	//code := e.INVALID_PARAMS
+	//if ok {
+	//	isExist := models.CheckAuth(username,password)
+	//	if isExist {
+	//		token, err := util.GenerateToken(username,password)
+	//		if err != nil{
+	//			code = e.ERROR_AUTH_TOKEN
+	//		}else {
+	//			data["token"] = token
+	//			code = e.SUCCESS
+	//		}
+	//	}else {
+	//		code = e.ERROR_AUTH
+	//	}
+	//}else {
+	//	for _,err :=range valid.Errors{
+	//		logging.Info(err.Key,err.Message)
+	//	}
+	//}
+	//c.JSON(http.StatusOK,gin.H{
+	//	"code":code,
+	//	"msg":e.GetMsg(code),
+	//	"data":data,
+	//})
+
+	appG := app.Gin{C: c}
+	valid := validation.Validation{}
+
 	username := c.Query("username")
 	password := c.Query("password")
 
-	valid := validation.Validation{}
-	a := auth{Username:username,Password:password}
-	ok, _:= valid.Valid(&a)
+	a := auth{Username: username, Password: password}
+	ok, _ := valid.Valid(&a)
 
-	data := make(map[string]interface{})
-	code := e.INVALID_PARAMS
-	if ok {
-		isExist := models.CheckAuth(username,password)
-		if isExist {
-			token, err := util.GenerateToken(username,password)
-			if err != nil{
-				code = e.ERROR_AUTH_TOKEN
-			}else {
-				data["token"] = token
-				code = e.SUCCESS
-			}
-		}else {
-			code = e.ERROR_AUTH
-		}
-	}else {
-		for _,err :=range valid.Errors{
-			logging.Info(err.Key,err.Message)
-		}
+	if ! ok {
+		app.MarkErrors(valid.Errors)
+		appG.Response(http.StatusBadRequest, e.INVALID_PARAMS, nil)
+		return
 	}
-	c.JSON(http.StatusOK,gin.H{
-		"code":code,
-		"msg":e.GetMsg(code),
-		"data":data,
+
+	authService := auth_service.Auth{Username: username, Password: password}
+	isExist, err := authService.Check()
+	if err != nil {
+		appG.Response(http.StatusInternalServerError, e.ERROR_AUTH_CHECK_TOKEN_FAIL, nil)
+		return
+	}
+
+	if ! isExist {
+		appG.Response(http.StatusUnauthorized, e.ERROR_AUTH, nil)
+		return
+	}
+	token, err := util.GenerateToken(username, password)
+	if err!= nil{
+		appG.Response(http.StatusInternalServerError,e.ERROR_AUTH_TOKEN,nil)
+		return
+	}
+
+	appG.Response(http.StatusOK, e.SUCCESS, map[string]string{
+		"token": token,
 	})
 }
